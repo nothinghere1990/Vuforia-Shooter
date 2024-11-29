71d8cc4a1261fd53764cc70d1613828ad499a609
nothinghere1990 <john1990xup6@gmail.com>
2024-11-29 19:09:59 +0800

    Weapon aiming and smoother rotation

--- Vuforia Shooter/Assets/Scripts/CharacterButton.cs
+++ Vuforia Shooter/Assets/Scripts/CharacterButton.cs
@@ -1,18 +1,34 @@
+using System;
 using UnityEngine;
 
 public class CharacterButton : MonoBehaviour
 {
+    private Transform chracterModel => transform.GetChild(0);
+    private Transform holdingItem;
+
+    private void Awake()
+    {
+        holdingItem = chracterModel.Find("HoldingItem");
+    }
+
     private void OnMouseUpAsButton()
     {
-        if (ButtonManager.Instance.clickedScriptableObject == null) return;
+        if (ButtonManager.Instance.clickedScriptableObject == null || transform.childCount < 1) return;
         
-        if (transform.childCount > 1) Destroy(transform.GetChild(1).gameObject);
         Weapon weapon = (Weapon)ButtonManager.Instance.clickedScriptableObject;
-        Instantiate(weapon.weaponModel, transform);
+        
+        Transform playerWeapon = Instantiate(weapon.weaponModel, chracterModel).transform;
+        playerWeapon.position = holdingItem.position;
+        playerWeapon.localScale = new Vector3(.2f, .2f, .2f);
+        if (chracterModel.childCount > 1) holdingItem.gameObject.SetActive(false);
     }
 
     public void OnWeaponLost()
     {
-        if (transform.childCount > 1) Destroy(transform.GetChild(1).gameObject);
+        if (chracterModel.childCount > 1)
+        {
+            holdingItem.gameObject.SetActive(false);
+            Destroy(chracterModel.GetChild(2).gameObject);
+        }
     }
 }
--- Vuforia Shooter/Assets/Scripts/PlayerController.cs
+++ Vuforia Shooter/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -6,7 +6,12 @@ public class PlayerController : MonoBehaviour
    private CharacterController charCon;
    private FixedJoystick joystick;
 
+   private Transform monster;
+   
+   private bool activeControl;
    private float moveSpeed;
+   private bool isAimTracking;
+   private float rotateSpeed;
 
    private void Awake()
    {
@@ -17,10 +22,49 @@ public class PlayerController : MonoBehaviour
    private void Start()
    {
       moveSpeed = 5;
+      rotateSpeed = 6;
+   }
+   
+   public void ActiveController(bool input)
+   {
+      StartCoroutine(ActiveControllerCoroutine(input));
+   }
+   IEnumerator ActiveControllerCoroutine(bool inputValue)
+   {
+      yield return new WaitForSeconds(0.5f);
+      activeControl = inputValue;
+   }
+
+   public void ActiveAimTracking(bool inputValue)
+   {
+      isAimTracking = inputValue;
+
+      monster = isAimTracking ? GameObject.Find("MonsterCard/Capsule").transform : null;
    }
 
    private void FixedUpdate()
    {
-      charCon.Move(new Vector3(joystick.Horizontal * moveSpeed * Time.fixedDeltaTime, 0, joystick.Vertical * moveSpeed * Time.fixedDeltaTime));
+      //Move
+      if (!charCon.enabled || !activeControl) return;
+      charCon.Move(new Vector3(joystick.Horizontal, 0, joystick.Vertical) * moveSpeed * Time.fixedDeltaTime);
+
+      //Aim
+      if (isAimTracking)
+      {
+         Vector3 aimDir = monster.position - transform.position;
+         Quaternion trackingLookAt = Quaternion.LookRotation(aimDir);
+         trackingLookAt = Quaternion.Slerp(transform.rotation, trackingLookAt, rotateSpeed * Time.fixedDeltaTime);
+         transform.rotation = trackingLookAt;
+         transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+         return;
+      }
+      
+      //Rotate
+      if (joystick.Horizontal == 0 && joystick.Vertical == 0) return;
+      
+      Quaternion lookAt = Quaternion.LookRotation(charCon.velocity);
+      lookAt = Quaternion.Slerp(transform.rotation, lookAt, rotateSpeed * Time.fixedDeltaTime);
+      transform.rotation = lookAt;
+      transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
    }
 }
